6a3b82b600fbf4026fb4a3c3f7c596c39b82d64f
nasim0994 <nasimuddin0994@gmail.com>
2023-09-20 14:20:45 +0600

    bulk jobs get slove

--- controllers/job.controllers.js
+++ controllers/job.controllers.js
@@ -16,6 +16,24 @@ exports.getJobs = async (req, res) => {
   }
 };
 
+exports.getBulkJobs = async (req, res) => {
+  try {
+    const result = await Jobs.find({
+      _id: JSON.parse(req.query.ids),
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error: error.message,
+    });
+  }
+};
+
 exports.getJob = async (req, res) => {
   try {
     const result = await Jobs.findOne({ _id: req.params.id });
--- routes/job.routes.js
+++ routes/job.routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getJobs, getJob } = require("../controllers/job.controllers");
+const {
+  getJobs,
+  getJob,
+  getBulkJobs,
+} = require("../controllers/job.controllers");
 
-router.get("/all-jobs", getJobs);
-router.get("/:id", getJob);
+// router.get("/all-jobs", getJobs);
+router.get("/bulk-jobs", getBulkJobs);
+// router.get("/:id", getJob);
 
 module.exports = router;
